baa048facf2ab98ef5f893ce130fded17b167eac
Astral Cat <61272718+AstralCat1@users.noreply.github.com>
2021-04-18 15:14:07 +0100

    Update MathQuiz.bat

--- MathQuiz.bat
+++ MathQuiz.bat
@@ -1,8 +1,30 @@
 @echo off
 set questionnumber=0
+echo Which type of math do you want to do?
+echo Addition
+echo Subtraction
+echo Multiplication
+echo Division
+set /p typeanswer=
+if %typeanswer% == Addition goto addition
+if %typeanswer% == Subtraction goto subtraction
+if %typeanswer% == Multiplication goto multiplication
+if %typeanswer% == Division goto division
+:addition
+set type=+
+goto question
+:subtraction
+set type=-
+goto question
+:multiplication
+set type=*
+goto question
+:division
+set type=/
+goto question
 :question
 cls
-set question=%random%+%random%
+set question=%random%%type%%random%
 set /a questionnumber=%questionnumber%+1
 set /a answer2=%question%
 echo Question %questionnumber%
